6487c58c38956d2c7a203616a31e2f750ec6bd5d
y4v8 <y4v8@yandex.ua>
2017-02-21 15:53:58 +0200

    Added handling for textarea

--- content-script.js
+++ content-script.js
@@ -8,11 +8,11 @@ let translate = document.URL.startsWith(translateURL);
 let portBS = chrome.runtime.connect({
   name: portName
 });
-let eventKeyTime,
+let eventTimeDown,
     eventKeyDown = false;
 
 if (translate) {
-  chrome.runtime.onMessage.addListener((msg, sender, resp) => {
+  chrome.runtime.onMessage.addListener(function(msg, sender, resp) {
     let id = 'extensionId' in sender ? sender.extensionId : sender.id;
     if (id != chrome.runtime.id) {
       return;
@@ -27,28 +27,37 @@ if (translate) {
   });
 }
 
-document.addEventListener('keydown', event => {
+function eventActive() {
+  if (eventKeyDown == false) {
+    return false;
+  }
+  
+  let now = new Date();
+  if (now - eventTimeDown > eventMaxDuration) {
+    return false;
+  }
+  
+  return true;
+}
+
+function eventKeydown(event) {
   if (event.key === eventKey) {
     if (eventKeyDown == false) {
-      eventKeyTime = new Date();
+      eventTimeDown = new Date();
       eventKeyDown = true;
     }
     return;
   }
   eventKeyDown = false;
-});
+}
 
-document.addEventListener('keyup', event => {
-  if (event.key !== eventKey || eventKeyDown == false) {
-    return;
-  }
+function eventKeyup(event) {
+  let active = eventActive();
   eventKeyDown = false;
-
-  let now = new Date();
-  if (now - eventKeyTime > eventMaxDuration) {
+  if (active == false) {
     return;
   }
-
+  
   if (translate) {
     portBS.postMessage({
       content: ''
@@ -56,10 +65,45 @@ document.addEventListener('keyup', event => {
     return;
   }
 
-  let data = window.getSelection().toString().trim();
+  let data;
+  if (event.target == document.body) {
+    data = window.getSelection().toString().trim();
+  } else {
+    data = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd).trim();
+  }
   if (data != '') {
     portBS.postMessage({
       content: data
     });
   }
+}
+
+document.addEventListener('keydown', eventKeydown);
+document.addEventListener('keyup', eventKeyup);
+
+let elements = document.getElementsByTagName('textarea');
+for (let i=0; i<elements.length; i++) {
+  elements[i].addEventListener('keydown', eventKeydown);
+  elements[i].addEventListener('keyup', eventKeyup);
+}
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    mutation.addedNodes.forEach(function(node) {
+      if (node.type == 'textarea') {
+        node.addEventListener('keydown', eventKeydown);
+        node.addEventListener('keyup', eventKeyup);
+      }
+    });
+    mutation.removedNodes.forEach(function(node) {
+      if (node.type == 'textarea') {
+        node.removeEventListener('keydown', eventKeydown);
+        node.removeEventListener('keyup', eventKeyup);
+      }
+    });
+  });    
 });
+
+var config = { childList: true, subtree: true };
+
+observer.observe(document.body, config);
